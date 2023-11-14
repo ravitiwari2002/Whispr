1d62961302f2e97c4146695ffd14fae67bbdc44d
Ravi Tiwari <ravitiwari2002@gmail.com>
2023-11-14 15:16:47 -0700

    completed Whispr App

---
+++ chat_app/lib/service/auth_service.dart
@@ -0,0 +1,49 @@
+import 'package:chat_app/helper/helper_function.dart';
+import 'package:chat_app/service/database_service.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+
+//login
+  Future loginWithEmailandPassword(String email, String password) async {
+    try {
+      User user = (await firebaseAuth.signInWithEmailAndPassword(
+              email: email, password: password))
+          .user!;
+      if (user != null) {
+        return true;
+      }
+    } on FirebaseAuthException catch (e) {
+      return e.message;
+    }
+  }
+
+//register
+  Future registerUserWithEmailandPassword(
+      String fullName, String email, String password) async {
+    try {
+      User user = (await firebaseAuth.createUserWithEmailAndPassword(
+              email: email, password: password))
+          .user!;
+      if (user != null) {
+        //call database service to update data
+        await DatabaseService(uid: user.uid).savingUserData(fullName, email);
+        return true;
+      }
+    } on FirebaseAuthException catch (e) {
+      return e.message;
+    }
+  }
+
+//signout
+  Future signOut() async {
+    try {} catch (e) {
+      await HelperFunctions.saveUserLoggedInStatus(false);
+      await HelperFunctions.saveUserEmailSF("");
+      await HelperFunctions.saveUserNameSF("");
+      await firebaseAuth.signOut();
+      return null;
+    }
+  }
+}
---
+++ chat_app/lib/shared/Constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:1044204359770:web:fed4c6246d070c8141c863";
+  static String apiKey = "AIzaSyB4W-IcGWrssAIkEb0Zp0Y5tmikHHLVNL0";
+  static String messagingSenderId = "1044204359770";
+  static String projectId = "chatappflutter-93979";
+  final primaryColor = const Color(0x006666);
+}
